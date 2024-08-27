7b1ee06805e2cff32dcd52e63aa04cb6b26bfcf6
lijianfeng <2634307978@qq.com>
2024-08-27 17:50:44 +0800

    人物移动/翻转/跳跃

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,12 +7,22 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public PlayerInputControl inputControl;
+    
+    public Vector2 inputDirection;
+
+    public float speed;
+    public float jumpFore;
+    
+    private Rigidbody2D rb;
+
 
     //比Start()先执行
     private void Awake()
     {
+        rb = GetComponent<Rigidbody2D>();
         //创建一个实例
         inputControl = new PlayerInputControl();
+        inputControl.GamePlay.Jump.started += Jump;
     }
 
     //当组件被启动的时候
@@ -28,15 +38,37 @@ public class PlayerController : MonoBehaviour
         inputControl.Disable();
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+        inputDirection = inputControl.GamePlay.Move.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        rb.velocity = new Vector2(inputDirection.x * speed * Time.deltaTime, rb.velocity.y);
+
+        int faceDir = (int)transform.localScale.x;
+        if (inputDirection.x > 0)
+        {
+            faceDir = 1;
+        }
+        if (inputDirection.x < 0)
+        {
+            faceDir = -1;
+        }
+        //人物翻转
+        transform.localScale = new Vector3(faceDir, 1, 1);
+    }
+    
+    //跳跃
+    private void Jump(InputAction.CallbackContext obj)
+    {
+        rb.AddForce(transform.up * jumpFore,ForceMode2D.Impulse);
     }
 }
