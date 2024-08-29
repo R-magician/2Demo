bd47b05ca3f56be03329f568463a1ff631117584
lijianfeng <2634307978@qq.com>
2024-08-29 14:03:42 +0800

    创建人物基本动画（跑走之间的切换需要用手柄）

---
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator anim;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        //获取到引用组件
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        SetAnimator();
+    }
+
+    //动画切换
+    public void SetAnimator()
+    {
+        anim.SetFloat("velocityX",MathF.Abs(rb.velocity.x));
+    }
+}
