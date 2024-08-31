7c6486ec8a8b0d176455c8b49995f724e7a154c9
lijianfeng <2634307978@qq.com>
2024-08-31 16:46:26 +0800

    野猪 - 撞墙判定和等候计时

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     protected Rigidbody2D rb;
-    protected Animator anim; 
+    protected Animator anim;
+    private PhysicsCheck physicsCheck;
     
     [Header("基本参数")]
     //基本的移动速度
@@ -17,17 +18,38 @@ public class Enemy : MonoBehaviour
     public float currentSpeed;
     //移动的方向
     public Vector3 faceDir;
-
+    
+    [Header("计时器")]
+    //等待时间
+    public float waitTime;
+    //
+    public float waitTimeConter;
+    //等待状态
+    public bool wait;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        physicsCheck = GetComponent<PhysicsCheck>();
         currentSpeed = normalSpeed;
+        waitTimeConter = waitTime;
     }
 
     private void Update()
     {
-        faceDir = new Vector3(-transform.lossyScale.x, 0, 0);
+        //获取移动的方向
+        faceDir = new Vector3(-transform.localScale.x, 0, 0);
+
+        //如果碰到两侧的墙，角色翻转
+        if ((physicsCheck.touchLeftWall && faceDir.x < 0) || (physicsCheck.touchRightWall && faceDir.x>0))
+        {
+            wait = true;
+            //关闭走路，开始等待
+            anim.SetBool("walk",false);
+        }
+
+        //延时转身
+        TimeCounter();
     }
 
     private void FixedUpdate()
@@ -39,5 +61,20 @@ public class Enemy : MonoBehaviour
     public virtual void Move()
     {
         rb.velocity = new Vector2(currentSpeed * faceDir.x * Time.deltaTime, rb.velocity.y);
+    }
+    
+    //计时器
+    public void TimeCounter()
+    {
+        if (wait)
+        {
+            waitTimeConter -= Time.deltaTime;
+            if (waitTimeConter <= 0)
+            {
+                wait = false;
+                waitTimeConter = waitTime;
+                transform.localScale = new Vector3(faceDir.x, 1, 1);
+            }
+        }
     }
 }
--- Assets/Scripts/General/PhysicsCheck.cs
+++ Assets/Scripts/General/PhysicsCheck.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 
 public class PhysicsCheck : MonoBehaviour
 {
-    [Header("检测参数")]
+    private CapsuleCollider2D coll;
+    
+    [Header("检测参数")] 
+    //是否手动检测
+    public bool manual;
     //脚底位移插值
     public Vector2 bottomOffset;
+    //左侧偏移
+    public Vector2 leftOffset;
+    //右侧偏移
+    public Vector2 rightOffset;
     //检测范围
     public float checkRaduis;
     //碰撞到那一个layer
@@ -16,6 +24,22 @@ public class PhysicsCheck : MonoBehaviour
     [Header("状态")]
     //是触碰到地面
     public bool isGround;
+    //撞左墙
+    public bool touchLeftWall;
+    //撞右墙
+    public bool touchRightWall;
+
+    private void Awake()
+    {
+        coll = GetComponent<CapsuleCollider2D>();
+        if (!manual)
+        {
+            //自动的，非手动
+            //bounds-世界坐标碰撞体的范围
+            rightOffset = new Vector2((coll.bounds.size.x + coll.offset.x) / 2, coll.bounds.size.y / 2);
+            leftOffset = new Vector2(-rightOffset.x, rightOffset.y);
+        }
+    }
 
     // Update is called once per frame
     void Update()
@@ -28,11 +52,20 @@ public class PhysicsCheck : MonoBehaviour
     {
         //检测地面状态
         isGround = Physics2D.OverlapCircle((Vector2)transform.position + bottomOffset, checkRaduis, groundLayer);
+        
+        //墙体的判断
+        touchLeftWall = Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, checkRaduis, groundLayer);
+        touchRightWall = Physics2D.OverlapCircle((Vector2)transform.position + rightOffset, checkRaduis, groundLayer);
+        
     }
 
     private void OnDrawGizmosSelected()
     {
-        //来可视化这个技能的影响范围，确保其效果符合预期
+        //来可视化这个技能的影响范围，确保其效果符合预期--绘制地板检测范围
         Gizmos.DrawWireSphere((Vector2)transform.position + bottomOffset ,checkRaduis);
+        //绘制左侧检测范围
+        Gizmos.DrawWireSphere((Vector2)transform.position + leftOffset ,checkRaduis);
+        //绘制右侧检测范围
+        Gizmos.DrawWireSphere((Vector2)transform.position + rightOffset ,checkRaduis);
     }
 }
